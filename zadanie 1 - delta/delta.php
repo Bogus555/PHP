773198c4d84354a522d3389baec2fdb3d1050b19
Bogus555 <bogus55512@wp.pl>
2017-04-19 14:45:56 +0200

    Skończone zadanie z obliczania delty

--- zadanie 1 - delta/delta.php
+++ zadanie 1 - delta/delta.php
@@ -5,12 +5,25 @@
 	<meta charset="UTF-8">
 </head>
 <style>
+	
+	body{
+	background: rgb(240,249,255);
+	background: -moz-linear-gradient(top, rgba(240,249,255,1) 0%, rgba(203,235,255,1) 47%, rgba(161,219,255,1) 100%);
+	background: -webkit-linear-gradient(top, rgba(240,249,255,1) 0%,rgba(203,235,255,1) 47%,rgba(161,219,255,1) 100%);
+	background: linear-gradient(to bottom, rgba(240,249,255,1) 0%,rgba(203,235,255,1) 47%,rgba(161,219,255,1) 100%);
+	filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f0f9ff', endColorstr='#a1dbff',GradientType=0 );
+	}
+	
 	h1{
 		text-align: center;
 		margin-bottom: 30px;
 	}
 	
 	form{
+		text-align: center;
+	}
+	
+	p{
 		text-align: center;
 	}
 </style>
@@ -39,6 +52,8 @@
 
 	<input type="submit">
 </form>
+	
+	<p>Strona została stworzona przez Jędrzeja Boguszyńskiego</p>
 	
 </body>
 </head>
